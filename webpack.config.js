8e76e8370dff6321c602dbbc1342cceb41f472b6
Ajay Rawat <ajayrawat2998@gmail.com>
2020-10-22 12:37:09 +1300

    Add files via upload
    webpack.config.js is the file which configurate nodejs  to run application in Heroku.

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path')
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src', 'index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(jsx|js)$/,
+        include: path.resolve(__dirname, 'src'),
+        exclude: /node_modules/,
+        use: [{
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', {
+                "targets": "defaults" 
+              }],
+              '@babel/preset-react'
+            ]
+          }
+        }]
+      },
+      
+        {test: /\.(jpe?g|png|gif|svg)$/i, loader: "file-loader"},
+        {
+            test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+            use: [
+              {
+                loader: 'file-loader'
+              }
+            ]
+        },
+        {
+            test: /\.css$/i,
+            use: ['style-loader', 'css-loader'],
+        }
+    ]
+  }
+}
